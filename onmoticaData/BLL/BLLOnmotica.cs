5644a2f5c8368c3953399a1159d3020dc0319635
miguel5612 <miguelangelcu@ufps.edu.co>
2019-03-09 18:44:20 -0500

    Created methods for generate and save reports

--- onmoticaData/BLL/BLLOnmotica.cs
+++ onmoticaData/BLL/BLLOnmotica.cs
@@ -25,7 +25,26 @@ namespace airQ.App_Code
                 return DB.Database.SqlQuery<onUser>(strSQL).ToList();
             }
         }
-        
+        public List<personalData> getpersonalData()
+        {
+            using (onHubEntities DB = new onHubEntities())
+            {
+                string strSQL = "SELECT * FROM personalData WHERE id = " + usrId.ToString();
+                return DB.Database.SqlQuery<personalData>(strSQL).ToList();
+            }
+        }
+        public List<report> getReport(string inTopic, string name, int thingId, DateTime startDate)
+        {
+            using (onHubEntities DB = new onHubEntities())
+            {
+                string strSQL = "INSERT INTO report (name, thingId, usrId, startDate, field1Max, field2Max, field3Max, field4Max, field5Max, field6Max, field7Max, field8Max, field9Max, " + "field10Max, field11Max, field12Max, field13Max, field14Max, field15Max,field1Min, field2Min, field3Min, field4Min, field5Min, field6Min, field7Min, field8Min, field9Min, " +   "field10Min, field11Min, field12Min, field13Min, field14Min, field15Min, field1AVG, field2AVG, field3AVG, field4AVG, field5AVG, field6AVG, field7AVG, field8AVG, field9AVG" +  ", field10AVG, field11AVG, field12AVG, field13AVG, field14AVG, field15AVG)  SELECT " + name + " AS name, " + thingId.ToString() + " AS thingId, " + usrId.ToString() + " AS usrId, " + thingId.ToString() + "  As thingId, MAX(field1) as field1Max,MAX(field2) as field2Max,MAX(field3) as field3Max,MAX(field4) as field4Max,MAX(field5) as field5Max,MAX(field6) as field6Max,MAX(field7) as field7Max,MAX(field8) as field8Max,MAX(field9) as field9Max,MAX(field10) as field10Max,MAX(field11) as field11Max,MAX(field12) as field12Max, MAX(field13) as field13Max, MAX(field14) as field14Max, MAX(field15) as field15Max, MIN(field1) as field1Min,MIN(field2) as field2MIN,Min(field3) as field3Min,MIN(field4) as field4Min,MIN(field5) as field5Min,MIN(field6) as field6Min,MIN(field7) as field7Min,MIN(field8) as field8Min,MIN(field9) as field9Min,MIN(field10) as field10Min,MIN(field11) as field11Min,MIN(field12) as field12Min, MIN(field13) as field13Min, MIN(field14) as field14Min, MIN(field15) as field15Min, AVG(field1) as field1AVG,AVG(field2) as field2AVG,AVG(field3) as field3AVG,AVG(field4) as field4AVG,AVG(field5) as field5AVG,AVG(field6) as field6AVG,AVG(field7) as field7AVG,AVG(field8) as field8AVG,AVG(field9) as field9AVG,AVG(field10) as field10AVG,AVG(field11) as field11AVG,AVG(field12) as field12AVG, AVG(field13) as field13AVG, AVG(field14) as field14AVG, AVG(field15) as field15AVG FROM measurements WHERE inTopic = " + inTopic;
+                DB.Database.ExecuteSqlCommand(strSQL);
+
+                strSQL = "SELECT * FROM report WHERE inTopic = " + inTopic.ToString() + " ORDER BY startDate DESC";
+                return DB.Database.SqlQuery<report>(strSQL).ToList();
+            }
+        }
+
 
 
     }
