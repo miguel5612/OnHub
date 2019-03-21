1b65f4457d52a7576e362020a32cdc65a7465a3b
miguel5612 <miguelangelcu@ufps.edu.co>
2019-03-20 21:20:07 -0500

    Btn login and redirect ok

--- onHub/Site.Master.cs
+++ onHub/Site.Master.cs
@@ -11,7 +11,26 @@ namespace airQ
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["usrID"] == null)
+            {
+                Button1.Text = "Registrarse"; 
+            }
+            else
+            {
+                Button1.Text = "Cerrar sesion";
+            }
+        }
 
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(Session["usrID"]  == null)
+            {
+                Response.Redirect("/login");
+            }
+            else
+            {
+                Response.Redirect("/logout");
+            }
         }
     }
 }
